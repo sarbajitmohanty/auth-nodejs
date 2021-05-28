f845fa8a5535328dde66debd870932850aa7ae82
sarbajitmohanty <sarbajit.mohanty02@gmail.com>
2021-05-28 15:22:32 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
